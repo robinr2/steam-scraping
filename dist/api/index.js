7402448835fe6888c0614dc11a6a05ce090fa86d
robinr2 <roeschkemis@gmail.com>
2023-04-17 14:04:00 +0200

    reworked api

--- dist/api/index.js
+++ dist/api/index.js
@@ -10,16 +10,16 @@ const data = table.map((row) => ({
 }));
 const filteredData = data.filter((row) => row.highest_buy_order > 0 &&
     row.lowest_sell_order > 0 &&
-    row.lowest_sell_order < 70 &&
+    row.lowest_sell_order < 12 &&
     row.lowest_sell_order > 4.5);
 const sortedData = [...filteredData].sort((a, b) => {
     const marginA = +a.lowest_sell_order - (+a.highest_buy_order * 1.15 - 0.01);
     const marginB = +b.lowest_sell_order - (+b.highest_buy_order * 1.15 - 0.01);
     return marginB - marginA;
 });
+let a = 0;
 for (const row of sortedData) {
-    console.log(row.url);
-    const line = `${row.id};${row.url}\n`;
+    const line = `${a++};${row.id};${row.url}\n`;
     fs.appendFileSync('data/data-final-sorted.csv', line);
 }
 //# sourceMappingURL=index.js.map
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,32 +1,153 @@
+import express, { Request, Response } from 'express'
 import fs from 'fs'
 
-const rows = fs.readFileSync('data/data-final-1680505709744.csv', 'utf8').trim().split('\n')
-const table = rows.map((row) => row.split(';'))
-const data = table.map((row) => ({
-  id: row[0],
-  url: row[1],
-  highest_buy_order: row[2] ? +row[2] / 100 : 0,
-  lowest_sell_order: row[3] ? +row[3] / 100 : 0,
-  timestamp: row[4],
-}))
+const app = express()
+const PORT = 3000
+const ITEM_PATH = 'data/items.csv'
+const HEADERS = [
+  'id',
+  'url',
+  'highestBuyOrder',
+  'lowestSellOrder',
+  'sellsPerMonth',
+  'tracked',
+  'updatedAt',
+]
 
-const filteredData = data.filter(
-  (row) =>
-    row.highest_buy_order > 0 &&
-    row.lowest_sell_order > 0 &&
-    row.lowest_sell_order < 70 &&
-    row.lowest_sell_order > 4.5
-)
+type Item = {
+  id: string
+  url: string
+  highestBuyOrder: number
+  lowestSellOrder: number
+  sellsPerMonth: number
+  tracked: boolean
+  updatedAt: number
+}
 
-const sortedData = [...filteredData].sort((a, b) => {
-  const marginA = +a.lowest_sell_order - (+a.highest_buy_order * 1.15 - 0.01)
-  const marginB = +b.lowest_sell_order - (+b.highest_buy_order * 1.15 - 0.01)
+type UpdateBody = {
+  highestBuyOrder: Item['highestBuyOrder'] | undefined
+  lowestSellOrder: Item['lowestSellOrder'] | undefined
+}
 
-  return marginB - marginA
+app.use(express.json())
+
+function getFields(line: string) {
+  if (line.trim() === '') return null
+
+  const fields = line.split(';')
+  if (fields.length !== HEADERS.length) return null
+
+  return fields
+}
+
+function getLines() {
+  const data = fs.readFileSync(ITEM_PATH, 'utf8')
+  const lines = data.split('\n')
+  const headers = lines[0]
+  if (!headers) return null
+
+  const headerFields = getFields(headers)
+  if (!headerFields) return null
+  if (!headerFields.every((val, index) => val === HEADERS[index])) return lines
+
+  lines.shift()
+
+  return lines
+}
+
+function setLines(lines: string[]) {
+  const data = lines.join('\n')
+  fs.writeFileSync(ITEM_PATH, data, 'utf8')
+}
+
+function getItem(fields: string[]) {
+  const id = fields[0]
+  if (!id) return null
+
+  const url = fields[1]
+  if (!url) return null
+
+  const highestBuyOrder = fields[2] ? +fields[2] / 100 : 0
+  const lowestSellOrder = fields[3] ? +fields[3] / 100 : 0
+  const sellsPerMonth = fields[4] ? +fields[4] : 0
+  const tracked = fields[5] === 'true'
+  const updatedAt = fields[6] ? +fields[6] : 0
+
+  const item = {
+    id,
+    url,
+    highestBuyOrder,
+    lowestSellOrder,
+    sellsPerMonth,
+    tracked,
+    updatedAt,
+  }
+
+  return item
+}
+
+app.get('/items', (req: Request, res: Response) => {
+  const lines = getLines()
+  if (!lines) {
+    res.sendStatus(404)
+    return
+  }
+
+  const items: Item[] = []
+  for (const line of lines) {
+    const fields = getFields(line)
+    if (!fields) continue
+
+    const item = getItem(fields)
+    if (!item) continue
+
+    items.push(item)
+  }
+
+  res.json(items)
 })
 
-for (const row of sortedData) {
-  console.log(row.url)
-  const line = `${row.id};${row.url}\n`
-  fs.appendFileSync('data/data-final-sorted.csv', line)
-}
+app.post('/items/:id', (req: Request, res: Response) => {
+  const { id } = req.params
+  if (!id) {
+    res.sendStatus(404)
+    return
+  }
+
+  const lines = getLines()
+  if (!lines) {
+    res.sendStatus(404)
+    return
+  }
+
+  let updated = false
+
+  for (let line of lines) {
+    const fields = getFields(line)
+    if (!fields || fields[0] !== id) continue
+
+    const item = getItem(fields)
+    if (!item) continue
+
+    const { highestBuyOrder, lowestSellOrder }: UpdateBody = req.body
+
+    item.highestBuyOrder = highestBuyOrder ?? item.highestBuyOrder
+    item.lowestSellOrder = lowestSellOrder ?? item.lowestSellOrder
+    item.updatedAt = Date.now()
+
+    line = `${item.id};${item.url};${item.highestBuyOrder};${item.lowestSellOrder};${item.sellsPerMonth};${item.tracked};${item.updatedAt}`
+    updated = true
+  }
+
+  if (!updated) {
+    res.sendStatus(404)
+    return
+  }
+
+  setLines(lines)
+  res.sendStatus(200)
+})
+
+app.listen(PORT, () => {
+  console.log(`API is listening on port ${PORT}`)
+})
